826ee020c442e853ee6d5dbea69c01a52a633eb6
Zack F <zackaryfotheringham@hotmail.com>
2023-02-02 11:19:52 -0500

    import components for game loop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import { useGameStatus } from "@/useGameStore";
+import EndModal from "./components/EndModal";
+import StartModal from "./components/StartModal";
+import Start from "./Start";
 
 export default function Home() {
+  const gameStatus = useGameStatus((state) => state.gameStatus);
   return (
-    <>
-    </>
+    <div className="text-gray-300">
+      {gameStatus === "start-menu" ? <StartModal /> : ""}
+      {gameStatus === "end-menu" ? <EndModal /> : ""}
+      {gameStatus === "playing" ? <Start /> : ""}
+    </div>
   )
 }
