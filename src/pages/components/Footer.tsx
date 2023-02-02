06e64f4ea45e13f200df79950101b9dff9915d61
Zack F <zackaryfotheringham@hotmail.com>
2023-02-02 11:24:07 -0500

    create footer for art attribution

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Footer():JSX.Element {
+  return (
+    <footer className="w-full py-2 bg-black flex justify-center gap-2">
+      Art By:
+      <a href="https://www.instagram.com/_itspear/" className="text-blue-500">
+        Pear
+      </a>
+    </footer>
+  )
+}
